8f5f441d6a64498404fa9f52edc4f063d548f8f7
Aetheris743 <45411688+Aetheris743@users.noreply.github.com>
2023-04-16 16:45:56 -0600

    Add tmp `get_winner`

--- src/main.rs
+++ src/main.rs
@@ -47,8 +47,46 @@ fn board_full(board: [[u32; 3]; 3]) -> bool {
 }
 
 fn get_winner(board: [[u32; 3]; 3]) -> i32 {
-    println!("Who won?");
-    0  // 0 if none, 1 if Player, 2 if computer.
+    // 0 if none, 1 if Player, 2 if computer.
+    // Check rows.
+    for row in board {
+        if row[0] == row[1] && row[1] == row[2] {
+            if row[0] == 1 {
+                return 1;
+            }
+            else if row[0] == 2 {
+                return 2;
+            }
+        }
+    }
+    // Check columns.
+    for i in 0..3 {
+        if board[0][i] == board[1][i] && board[1][i] == board[2][i] {
+            if board[0][i] == 1 {
+                return 1;
+            }
+            else if board[0][i] == 2 {
+                return 2;
+            }
+        }
+    }
+    // Check diagonals.
+    if board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+        if board[0][0] == 1 {
+            return 1;
+        }
+        else if board[0][0] == 2 {
+            return 2;
+        }
+    }
+    if board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+        if board[0][2] == 1 {
+            return 1;
+        }
+        else if board[0][2] == 2 {
+            return 2;
+        }
+    }
 }
 
 fn player_move(board: [[u32; 3]; 3]) {
